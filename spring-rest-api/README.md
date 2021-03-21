4db7e27ced97e5503a414208c53f5b1c2458f1c2
Gleyson Sampaio <glysns@hotmail.com>
2021-03-21 12:57:39 -0300

    api rest

---
+++ spring-rest-api/README.md
@@ -0,0 +1,20 @@
+# Digytal - Treinamento, Consultoria e Fábrica de Software
+www.digytal.com.br
+(11) 95894 0362
+
+# Java Exemplos - Spring Rest API
+
+Projeto open source para demonstrar recursos API Rest com Springboot
+
+> Este repositório tem como finalidade apresentar a disponibilização de recursos HTTP para protocolo REST utilizando Springboot
+
+- Execute a classe `digytal.java.exemplos.springrestapi.SpringRestApiApplication`.
+
+| Pacote              |  Descrição  |
+| ------------------- | ------------------- |
+|  requesttype |  Exemplo de uso dos tipos de requisição, verbos, parametros e corpo |
+|  Célula de conteúdo |  Célula de conteúdo |
+
+
+- Referências:
+stackabuse.com - https://stackabuse.com/java-rest-api-documentation-with-swagger2/#:~:text=What%20is%20Swagger%3F,documents%20for%20RESTful%20web%20services.
---
+++ spring-rest-api/src/main/java/digytal/java/exemplos/springrestapi/resource/requesttype/RequestType.java
@@ -0,0 +1,70 @@
+package digytal.java.exemplos.springrestapi.resource.requesttype;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import digytal.java.exemplos.springrestapi.model.Endereco;
+import digytal.java.exemplos.springrestapi.repository.FakeRepository;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@RestController
+@RequestMapping("/request-type/enderecos")
+public class RequestType {
+	@Autowired
+	private FakeRepository repository;
+	
+	@ApiOperation(value = "Retorna uma lista de endereços")
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Operação realizada com sucesso")
+	})
+	@GetMapping
+	public List<Endereco> get(){
+		return repository.findAll();
+	}
+	
+	@ApiOperation(value = "Adiciona um endereço")
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Operação realizada com sucesso")
+	})
+	@PostMapping
+	public void post(@RequestBody Endereco endereco){
+		repository.save(endereco);
+	}
+	
+	@ApiOperation(value = "Altera um endereço")
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Operação realizada com sucesso")
+	})
+	@PutMapping
+	public void put(@RequestBody Endereco endereco){
+		repository.update(endereco);
+	}
+	@ApiOperation(value = "Remove um endereço passando o cep na URL ex.: /enderecos/65300123")
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Operação realizada com sucesso")
+	})
+	@DeleteMapping(path = "/{cep}")
+	public void delete(@PathVariable("cep") String cep){
+		repository.remove(cep);
+	}
+	@ApiOperation(value = "Remove um endereço passando cep o como parametro ex.: /enderecos/param?cep=65300123")
+	@ApiResponses(value = {
+	        @ApiResponse(code = 200, message = "Operação realizada com sucesso")
+	})
+	@DeleteMapping(path = "/param")
+	public void deleteParam(@RequestParam("cep") String cep){
+		repository.remove(cep);
+	}
+}
