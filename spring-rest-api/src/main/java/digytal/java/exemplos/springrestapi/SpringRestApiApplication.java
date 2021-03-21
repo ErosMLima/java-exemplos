912472ac6ee0255e8fc3df43392050beafa157e1
Gleyson Sampaio <glysns@hotmail.com>
2021-03-21 20:48:34 -0300

    Update SpringRestApiApplication.java

--- spring-rest-api/src/main/java/digytal/java/exemplos/springrestapi/SpringRestApiApplication.java
+++ spring-rest-api/src/main/java/digytal/java/exemplos/springrestapi/SpringRestApiApplication.java
@@ -13,14 +13,5 @@ public class SpringRestApiApplication {
 		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
 	
-	@Bean
-	public WebMvcConfigurer corsConfigurer() {
-		return new WebMvcConfigurer() {
-			@Override
-			public void addCorsMappings(CorsRegistry registry) {
-				registry.addMapping("/**").allowedOrigins("http://localhost:8080");
-			}
-		};
-	}
 
 }
