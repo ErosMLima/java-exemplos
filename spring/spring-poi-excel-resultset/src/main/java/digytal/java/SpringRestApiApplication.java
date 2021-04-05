f6febc2340b133440d005ddce46cc9e497beec58
Gleyson Sampaio <glysns@hotmail.com>
2021-04-04 21:48:49 -0300

    imagens

--- spring/spring-poi-excel-resultset/src/main/java/digytal/java/SpringRestApiApplication.java
+++ spring/spring-poi-excel-resultset/src/main/java/digytal/java/SpringRestApiApplication.java
@@ -18,38 +18,32 @@ public class SpringRestApiApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringRestApiApplication.class, args);
 	}
-	
 	@Bean
 	public CommandLineRunner run() throws Exception {
 		return args -> {
 			
-			try {
-				ClassPathResource resourceFile = new ClassPathResource("transformadores.xlsx");
-				File file =  resourceFile.getFile();
-				
-				ExcelResultSet exRs = new ExcelResultSet(file);
-				
-				while(exRs.next()) {
-					String cpf = exRs.getString("Cpf");
-					String nome = exRs.getString("Nome");
-					LocalDate dtNascimento = exRs.getLocalDate("Data Nascimento");
-					Sexo sexo = Sexo.valueOf(exRs.getString("Sexo").toUpperCase());
-					Double valorHora = exRs.getDouble("Valor Hora");
-					Boolean brasileiro = exRs.getBoolean("Brasileiro?", "S");
-					
-					System.out.println(String.format("O professor %s de Cpf %s, nascido em %s, no País %s do Sexo %s, com valor hora R$ %.2f ",
-							nome,
-							cpf,
-							dtNascimento.toString(),
-							brasileiro?"BRASIL":"EXTERIOR",
-							sexo.getDescricao(),
-							valorHora
-							));	
-				}
-			} catch (Exception e) {
-				e.printStackTrace();
-			}
+			ClassPathResource resourceFile = new ClassPathResource("transformadores.xlsx");
+			File file =  resourceFile.getFile();
 			
+			ExcelResultSet exRs = new ExcelResultSet(file);
+			
+			while(exRs.next()) {
+				String cpf = exRs.getString("Cpf");
+				String nome = exRs.getString("Nome");
+				LocalDate dtNascimento = exRs.getLocalDate("Data Nascimento");
+				Sexo sexo = Sexo.valueOf(exRs.getString("Sexo").toUpperCase());
+				Double valorHora = exRs.getDouble("Valor Hora");
+				Boolean brasileiro = exRs.getBoolean("Brasileiro?", "S");
+				
+				System.out.println(String.format("O professor %s de Cpf %s, nascido em %s, no País %s do Sexo %s, com valor hora R$ %.2f ",
+						nome,
+						cpf,
+						dtNascimento.toString(),
+						brasileiro?"BRASIL":"EXTERIOR",
+						sexo.getDescricao(),
+						valorHora
+						));	
+			}	
 		};
 	}
 
--- spring/spring-poi-excel-resultset/src/main/java/digytal/java/model/Endereco.java
+++ spring/spring-poi-excel-resultset/src/main/java/digytal/java/model/Endereco.java
@@ -1,17 +1,10 @@
 package digytal.java.model;
 
-import io.swagger.annotations.ApiModel;
-import io.swagger.annotations.ApiModelProperty;
 
-@ApiModel
 public class Endereco {
-	@ApiModelProperty(notes = "Número do CEP", name = "Cep", required = true,dataType = "string", example = "65300000" )
 	private String cep;
-	@ApiModelProperty(notes = "Nome da Rua, Avenida", name = "Logradouro", required = true,dataType = "string", example = "Praça da Sé" )
 	private String logradouro;
-	@ApiModelProperty(notes = "Bairro do endereço", name = "Bairro", required = true,dataType = "string", example = "Sé" )
 	private String bairro;
-	@ApiModelProperty(notes = "Nome da Cidade", name = "Localidade", required = true,dataType = "string", example = "São Paulo" )
 	private String localidade;
 	public Endereco() {
 		
