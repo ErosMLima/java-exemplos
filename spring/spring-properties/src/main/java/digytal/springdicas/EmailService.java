d56621a8e7025bdd69d6a8a976eecbf4101825c4
Gleyson Gama <glysns@hotmail.com>
2021-04-07 22:40:19 -0300

    Projeto Spring Properties

---
+++ spring/spring-properties/src/main/java/digytal/springdicas/EmailService.java
@@ -0,0 +1,21 @@
+package digytal.springdicas;
+
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+public class EmailService {
+	@Value("${remetente}")
+	//@Value("${remetente:gleyson@digytal.com.br}")
+	private String remetente;
+	
+	@Value("${destinatarios}")
+	private List<String>destinatarios;
+	
+	public void enviar() {
+		System.out.println("Remetente:" + remetente);
+		System.out.println("Destinatarios:" + destinatarios);
+	}
+}
---
+++ spring/spring-properties/src/main/java/digytal/springdicas/FtpCredencial.java
@@ -0,0 +1,39 @@
+package digytal.springdicas;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.context.annotation.Configuration;
+
+
+@Configuration
+@ConfigurationProperties(prefix = "ftp")
+public class FtpCredencial {
+	private String host;
+	private String user;
+	private String pass;
+	private Integer port;
+	public String getHost() {
+		return host;
+	}
+	public void setHost(String host) {
+		this.host = host;
+	}
+	public String getUser() {
+		return user;
+	}
+	public void setUser(String user) {
+		this.user = user;
+	}
+	public String getPass() {
+		return pass;
+	}
+	public void setPass(String pass) {
+		this.pass = pass;
+	}
+	public Integer getPort() {
+		return port;
+	}
+	public void setPort(Integer port) {
+		this.port = port;
+	}
+	
+}
